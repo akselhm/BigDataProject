52bd3593390f012a854e62c3aea2f4947a50ed6f
Håkon Pelsholen Busterud <haakonbusterud@gmail.com>
2020-03-02 11:58:48 +0100

    Set data folder and input files

--- task1.py
+++ task1.py
@@ -1,2 +1,10 @@
 from pyspark import SparkContext, SparkConf
 
+sparkConf = SparkConf().setAppName("Yelp").setMaster("local")
+sc = SparkContext(conf = sparkConf)
+folder_name = "./data/"
+input_businesses = "yelp_businesses.csv.gz"
+input_reviewers = "yelp_top_reviewers_with_reviews.csv.gz"
+input_users = "yelp_top_users_friendship_graph.csv.gz"
+output_file_name = "result_1.tsv"
+
