56041ced05e637951a4f076d3199573b5a319767
Aksel Madslien <aksel-96@hotmail.com>
2020-03-02 12:08:31 +0100

    made RDDs task1

--- task1.py
+++ task1.py
@@ -2,9 +2,15 @@ from pyspark import SparkContext, SparkConf
 
 sparkConf = SparkConf().setAppName("Yelp").setMaster("local")
 sc = SparkContext(conf = sparkConf)
+
 folder_name = "./data/"
 input_businesses = "yelp_businesses.csv.gz"
 input_reviewers = "yelp_top_reviewers_with_reviews.csv.gz"
 input_users = "yelp_top_users_friendship_graph.csv.gz"
 output_file_name = "result_1.tsv"
+
+businessesRDD = sc.textFile(folder_name + input_businesses)
+reviewersRDD = sc.textFile(folder_name + input_reviewers)
+usersRDD = sc.textFile(folder_name + input_users)
+
 
