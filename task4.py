da18e30753c3c6249f6c47d0570ceafa08a96ff4
Aksel Madslien <aksel-96@hotmail.com>
2020-03-06 12:35:44 +0100

    minor changes task4

--- task4.py
+++ task4.py
@@ -15,17 +15,17 @@ users_lines_rdd = usersRDD.map(lambda line: line.split('\t'))
 
 # TODO: return only top ten (topByKey?)
 
-most_in = users_lines_rdd.map(lambda node: (node[1], 1)).reduceByKey(add).sortByValue()
-top_ten_in = most_in.topByKey(10)
+most_in = users_lines_rdd.map(lambda node: (node[1], 1)).reduceByKey(add)
+top_ten_in = most_in.takeOrdered(10, lambda x: -x[1])
 
-most_out = users_lines_rdd.map(lambda node: (node[0], 1)).reduceByKey(add).sortByValue()
-top_ten_out = most_out.topByKey(10)
+most_out = users_lines_rdd.map(lambda node: (node[0], 1)).reduceByKey(add)
+top_ten_out = most_out.takeOrdered(10, lambda x: -x[1])
 
 # b) TODO: find mean and median of users in and out
 
 # FIND MEAN
 
-avg_in = float(users_lines_rdd.count())/most_in.count()
+avg_in = float(users_lines_rdd.count())/most_in.count() #tot conections/users with connections "in"
 avg_out = float(users_lines_rdd.count())/most_out.count()
 
 # FIND MEDIAN
